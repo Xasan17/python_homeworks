7977364a81e6aa8b328f98cc632affb7d75d2e0f
Xasan17 <161180129+Xasan17@users.noreply.github.com>
2025-05-14 14:29:32 +0500

    Update hw_1.py

--- lesson-1/homework/hw_1.py
+++ lesson-1/homework/hw_1.py
@@ -1,1 +1,30 @@
+#1
+a=float(input('first side='))
+b=float(input('second side='))
+P=2*(a+b)
+print(P)
+#2
+import math
+d=float(input('diameter='))
+C = math.pi*d
+print(C)
+#3
+a=float(input('first number='))
+b=float(input('second number='))
+c=(a+b)/2
+print(c)
+#4
+a=float(input('first number='))
+b=float(input('second number='))
+sum= a+b
+mul=a*b
+asq= a**2
+bsq= b**2
+print(
+    f'sum:{sum} \n'
+    f'multiplication:{mul} \n'
+    f'square a and b: {asq} and {bsq}'
+)
+
+
 
